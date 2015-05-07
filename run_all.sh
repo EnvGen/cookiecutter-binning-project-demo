96b443a876ef581f8c11c45f7189a35cf044ba78
Johannes Alneberg <johannesalneberg@gmail.com>
2015-05-07 15:23:26 +0200

    Fix broken links to data

--- run_all.sh
+++ run_all.sh
@@ -4,9 +4,10 @@ cookiecutter https://github.com/EnvGen/cookiecutter-binning-project --no-input
 cd binning-project
 
 mkdir data
-
-ln -s ../contigs data/
-ln -s ../reads data/
+cd data
+ln -s ../../contigs .
+ln -s ../../reads .
+cd ..
 
 snakemake --list-target-rules
 snakemake --dryrun concoct_inputtable_10K_all
